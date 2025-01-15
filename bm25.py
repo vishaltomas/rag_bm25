6fc57d347889316660a6c01be24bd00023b25dee
Vishal Thomas <vishal.thomas.official@gmail.com>
2025-01-15 06:44:44 +0000

    Added queue in  bm25

--- bm25.py
+++ bm25.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from concurrent.futures import ThreadPoolExecutor
 import nltk
 import os
+from queue import Queue
 
 def _safety_import_():
     # Check whether imported modules work fine
@@ -26,7 +27,7 @@ class Word:
     """Contains name of the word its frequency and inverse frequency across documents"""
     name: str
     freq: int = 0
-    ifreq: int = 0 
+    doc: list = []
     
 class BM25:
     def __init__(self, num_docs, file_ls):
@@ -34,11 +35,23 @@ class BM25:
         self.files = file_ls
         # Key: token, Value: Word
         self.words = {}
+        self.words_queue = Queue()
         self.WORKER_THREADS = 10
     def collect_words(self):
+        while True:
+            words, doc = self.words_queue.get()
+            for word in words.keys():
+                self.words[word] = Word(
+                    name=word,
+                    freq=self.words[word].freq + sum(words[word]),
+                    doc=self.words[word].doc + [doc]
+                )
+            self.words_queue.task_done()
+
     def ext_metrics(self, file_path):
         rem_specials = lambda w: [c for c in w if w.isalnum()]
         vec_rem_sp = np.vectorize(rem_specials)
+        file_name = file_path.split("/")[-1]
         with open(file_path) as file:
             # Store words and its count
             words={}
@@ -47,7 +60,7 @@ class BM25:
             # Tokenize entire text into words
             word_ls = np.array(nltk.word_tokenize(data))
             # Remove all non-alnum characters
-            word_ls = wordls[word_ls.isalnum()]
+            word_ls = word_ls[word_ls.isalnum()]
             # Remove special characters within the words
             word_ls = vec_rem_sp(word_ls)
             # Count all the unique words in the count
@@ -56,13 +69,20 @@ class BM25:
             for index,word in enumerate(unique_words):
                 words[word] = self.words.get(word, [])
                 words[word].append(counts[index])
-            return words
+            self.words_queue.put((words, file_name))
+            return True
     def ext_docs(self):
         # Create a thread to execute collection of words and its counts
-        thread = threading.Thread(target = self.collect_words, daemon=True).start()
+        thread = threading.Thread(target = self.collect_words, daemon=True)
+        self.thread
+        thread.start()
         # Execute multiple threads
         with ThreadPoolExecutor(self.WORKER_THREADS) as executor:
             results = executor.map(self.ext_metrics, *self.files)
+        if not (sum[results] == len(self.files)):
+            print("Not all files are executed properly")
+        self.words_queue.join()
+
     def idf(self, qi, N):
         pass
     def bm25(self, qi, d, k1, b):
